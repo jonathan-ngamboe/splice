31ded18ba32cb59fdf6e773266c0d2c2dbfa6576
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2025-11-30 13:32:11 +0100

    feat: Add pdfbox dependency and implement basic text extraction using PDFTextStripper.

--- src/main/java/com/splice/service/PdfAnalyzer.java
+++ src/main/java/com/splice/service/PdfAnalyzer.java
@@ -1,14 +1,43 @@
 package com.splice.service;
 
 import com.splice.model.PageContent;
+import com.splice.model.TextContent;
+
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
 
 import java.io.IOException;
 import java.nio.file.Path;
+
+import java.util.LinkedList;
 import java.util.List;
 
 public class PdfAnalyzer implements DocumentAnalyzer{
     @Override
     public List<PageContent> analyze(Path path) throws IOException {
-        return List.of();
+        List<PageContent> documentContent = new LinkedList<>();
+
+        var file = path.toFile();
+        try (var document = Loader.loadPDF(file)) {
+            var pages = document.getPages();
+
+            for (int pageNumber = 1; pageNumber <= pages.getCount(); pageNumber++) {
+                String pageText = getPageText(document, pageNumber);
+                PageContent content = new TextContent(pageText, pageNumber);
+                documentContent.add(content);
+            }
+        }
+
+        return documentContent;
+    }
+
+    private String getPageText(PDDocument doc, int page) throws IOException {
+        var stripper = new PDFTextStripper();
+
+        stripper.setStartPage(page);
+        stripper.setEndPage(page);
+
+        return stripper.getText(doc);
     }
 }
