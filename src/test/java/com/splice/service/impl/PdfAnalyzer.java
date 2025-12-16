ba6fad440797b4313d609add577d43d970f11a0a
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2025-12-16 09:55:43 +0100

    feat: add utility method getResourcePath to make it easier to read the code when retrieving files

--- src/test/java/com/splice/service/impl/PdfAnalyzer.java
+++ src/test/java/com/splice/service/impl/PdfAnalyzer.java
@@ -15,16 +15,20 @@ import java.util.Objects;
 class PdfAnalyzerTests {
     private final DocumentAnalyzer analyzer = new PdfAnalyzer();
 
+    private Path getResourcePath(String filename) throws URISyntaxException {
+        return Paths.get(Objects.requireNonNull(getClass().getResource(filename)).toURI());
+    }
+
     @Test
     public void analyze_validPdf_shouldReturnNonEmptyList() throws IOException, URISyntaxException {
-        Path file = Paths.get(Objects.requireNonNull(getClass().getResource("/sample.pdf")).toURI());
+        Path file = getResourcePath("/sample.pdf");
         var result = analyzer.analyze(file);
         assertFalse(result.isEmpty(), "List should not be empty for a valid Pdf.");
     }
 
     @Test
     public void analyze_multiPagePdf_shouldReturnCorrectPageCount() throws IOException, URISyntaxException  {
-        Path file = Paths.get(Objects.requireNonNull(getClass().getResource("/three_pages.pdf")).toURI());
+        Path file = getResourcePath("/three_pages.pdf");
         var result = analyzer.analyze(file);
         assertEquals(3, result.size());
     }
@@ -39,7 +43,7 @@ class PdfAnalyzerTests {
 
     @Test
     public void analyze_invalidFormat_shouldThrowException() throws URISyntaxException {
-        Path file = Paths.get(Objects.requireNonNull(getClass().getResource("/fake.txt")).toURI());
+        Path file = getResourcePath("/fake.txt");
         assertThrows(IOException.class, () -> {
             analyzer.analyze(file);
         });
@@ -47,14 +51,14 @@ class PdfAnalyzerTests {
 
     @Test
     public void analyze_imagePdf_shouldReturnImageContent() throws IOException, URISyntaxException {
-        Path file = Paths.get(Objects.requireNonNull(getClass().getResource("/scanned.pdf")).toURI());
+        Path file = getResourcePath("/scanned.pdf");
         var result = analyzer.analyze(file);
         assertInstanceOf(ImageContent.class, result.getFirst());
     }
 
     @Test
     public void analyze_mixedPdf_shouldReturnBothContentTypes() throws IOException, URISyntaxException {
-        Path file = Paths.get(Objects.requireNonNull(getClass().getResource("/mixed.pdf")).toURI());
+        Path file = getResourcePath("/mixed.pdf");
         var result = analyzer.analyze(file);
         assertAll(
                 () -> assertEquals(2,
