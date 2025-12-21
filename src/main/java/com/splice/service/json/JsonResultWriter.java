ab60ebce27d8c59715d894b9bdc79b71641d9356
Jonathanngamboe <Jonathan.Ngamboe@he-arc.ch>
2025-12-21 15:06:19 +0100

    refactor(json): remove default filename generation if directory is provided

--- src/main/java/com/splice/service/json/JsonResultWriter.java
+++ src/main/java/com/splice/service/json/JsonResultWriter.java
@@ -8,16 +8,11 @@ import tools.jackson.databind.SerializationFeature;
 import tools.jackson.databind.json.JsonMapper;
 import tools.jackson.core.type.TypeReference;
 
-import java.nio.file.Files;
 import java.nio.file.Path;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 import java.util.List;
 
 public class JsonResultWriter implements ResultWriter {
-    private static final String DEFAULT_NAME = "splice_report";
     private static final String FILE_EXTENSION = ".json";
     private final ObjectMapper mapper;
 
@@ -28,17 +23,9 @@ public class JsonResultWriter implements ResultWriter {
     }
 
     @Override
-    public void write(List<PageContent> content, Path destination) {
-        Path finalFile = destination;
-
-        if(Files.isDirectory(destination)) {
-            String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss"));
-            String filename = DEFAULT_NAME + "_" + timestamp + FILE_EXTENSION;
-            finalFile = destination.resolve(filename);
-        }
-
+    public void write(List<PageContent> content, Path destinationFile) {
         mapper.writerFor(new TypeReference<List<PageContent>>() {})
-                .writeValue(finalFile.toFile(), content);
+                .writeValue(destinationFile.toFile(), content);
     }
 
     @Override
